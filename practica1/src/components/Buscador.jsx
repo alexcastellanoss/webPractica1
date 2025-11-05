83199d75f3a540fc33342cd9b9e3ea2f70654920
alexcastellanoss <ach.castellanos@gmail.com>
2025-11-05 18:31:10 +0100

    Buscador
    Buscador simple.
    Creamos una variable de estado query, que guarda lo que el usuario escribe.
    Cada vez que se presiona una tecla, el evento onChange actualiza query mediante setQuery.
    Cuando se envía el formulario, se ejecuta handleSubmit, que evita la recarga y envía el valor de query al componente padre con onSearch(query).

--- practica1/src/components/Buscador.jsx
+++ practica1/src/components/Buscador.jsx
@@ -1,16 +1,25 @@
 import '../styles/Buscador.css'
-import { IoSearch } from "react-icons/io5";
-
+import { IoSearch } from "react-icons/io5"
+import { useState } from 'react'
 
 export default function Buscador({ onSearch }) {
 
+    const [query, setQuery] = useState(' ')
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        onSearch(query)
+    }
+
     return (
-        <form className='form-buscador'>
+        <form className='form-buscador' onSubmit={handleSubmit}>
             <input
                 id='input'
                 className='input-buscador'
                 type='text'
                 placeholder='Buscar...'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
             />
             <button className='button' type='submit'>
                 <IoSearch id='icono' />
